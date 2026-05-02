e7a3c653026691ff02af04807a4894c04d9aa16b
ApostolLauraElena <laura-elena.apostol@student.tuiasi.ro>
2026-05-02 22:27:40 +0300

    laborator 10 tema2

--- app.js
+++ app.js
@@ -18,20 +18,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 app.get('/', (req, res) => res.send('Hello World'));
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
+const fs = require('fs');
+
 app.get('/chestionar', (req, res) => {
- const listaIntrebari = [
- { intrebare: 'Întrebarea 1',
- variante: ['varianta 1', 'varianta 2', 'varianta 3', 'varianta 4'],
- corect: 0
- },
- //...
+    const listaIntrebari = [
+        {
+
+        intrebare: 'Întrebarea 1',
+        variante: ['varianta 1', 'varianta 2', 'varianta 3', 'varianta 4'],
+        corect: 0
+        },
+        {
+            "intrebare": "Care dintre următoarele este considerat un electrocasnic mic pentru bucătărie?",
+            "variante": ["Congelatorul", "Mașina de spălat vase", "Prăjitorul de pâine", "Hota"],
+            "corect": 2
+        },
+        {
+            "intrebare": "Ce proces de curățare a cuptorului presupune arderea resturilor la temperaturi foarte înalte?",
+            "variante": ["Curățare catalitică", "Curățare manuală", "Curățare hidrolitică", "Curățare pirolitică"],
+            "corect": 3
+        },
+        {
+            "intrebare": "În ce unitate de măsură este exprimată de obicei puterea unui aspirator?",
+            "variante": ["Amperi", "Wați (W)", "Volți", "Lumeni"],
+            "corect": 1
+        }
  ];
- // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care
+
  res.render('chestionar', {intrebari: listaIntrebari});
 });
 app.post('/rezultat-chestionar', (req, res) => {
- console.log(req.body);
- res.send("formular: " + JSON.stringify(req.body));
+    console.log("Date primite de la formular:", req.body);
+
+    const intrebari = JSON.parse(fs.readFileSync('intrebari.json'));
+    let scor = 0;
+
+    intrebari.forEach((intrebare, index) => {
+        const raspunsUtilizator = req.body['intrebare-' + index];
+        if (raspunsUtilizator == intrebare.variante[intrebare.corect]) {
+            scor++;
+        }
+    });
+
+   res.render('rezultat-chestionar', { scor: scor, total: intrebari.length });
 });
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:
 :${port}/`));
