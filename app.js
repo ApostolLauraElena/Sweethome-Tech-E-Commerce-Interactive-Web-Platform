30bb6050d8305889be02068fe7e6ec7710262e43
ApostolLauraElena <laura-elena.apostol@student.tuiasi.ro>
2026-05-13 15:24:30 +0300

    lab 11 tema 3

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const cookieParser=require('cookie-parser'); 
+const session = require('express-session');
 const bodyParser = require('body-parser')
 const app = express();
+app.use(session({
+    secret: 'cheie-secreta',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(cookieParser());
+app.use((req, res, next) => { // Adaugă 'next' aici
+    res.locals.utilizator = req.session.utilizator || null;
+    next(); // Apelează 'next()' pentru a trece la următoarea rută
+});
 const port = 6789;
 // directorul 'views' va conține fișierele .ejs (html + js executat la server)
 app.set('view engine', 'ejs');
@@ -18,31 +28,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'HelloWorld'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-app.get('/', async(req, res) =>{ 
-    const username = req.cookies.utilizator;
-    res.render('index.ejs',{ utilizator: username } );
-})
+app.get('/', (req, res) => { 
+    res.render('index'); 
+});
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 const fs = require('fs');
-app.get('/autentificare', async(req, res) => {
-    const username = req.cookies.utilizator;
+app.get('/autentificare', (req, res) => {
     const mesajEroare = req.cookies.mesajEroare;
-    res.render('autentificare',{utilizator: username,  mesajEroare:mesajEroare}); 
+    res.clearCookie('mesajEroare'); 
+    res.render('autentificare', { mesajEroare: mesajEroare }); 
 });
-app.post('/verificare-autentificare', (req, res) => {
-    const utilizatorIntrodus = req.body.utilizator;
-    const parolaIntrodusa = req.body.parola;
+app.post('/verificare-autentificare', async (req, res) => {
+    const { utilizator, parola } = req.body;
 
-    console.log("Date primite la verificare:", req.body);
+    try {
+        const data = await fs.promises.readFile('utilizatori.json', 'utf8');
+        const utilizatori = JSON.parse(data);
 
-    if (utilizatorIntrodus === 'aaa' && parolaIntrodusa === 'aaa') { // Poți pune 'Ion' și '12345'
-        console.log("Autentificare reușită pentru:", utilizatorIntrodus);
-        res.cookie('utilizator', utilizatorIntrodus);
-        res.redirect('/');
-    } else {
-        console.log("Autentificare eșuată!");
-        res.cookie('mesajEroare', "Autentificare eșuată!");
-        res.redirect('/autentificare');
+        const utilizatorGasit = utilizatori.find(u => u.utilizator === utilizator && u.parola === parola);
+
+        if (utilizatorGasit) {
+            req.session.utilizator = {
+                username: utilizatorGasit.utilizator,
+                nume: utilizatorGasit.nume,
+                prenume: utilizatorGasit.prenume
+            };
+            res.redirect('/');
+        } else {
+            res.cookie('mesajEroare', "Utilizator sau parolă incorectă!");
+            res.redirect('/autentificare');
+        }
+    } catch (err) {
+        res.status(500).send("Eroare la verificarea utilizatorilor.");
     }
 });
 
@@ -75,10 +92,12 @@ app.post('/rezultat-chestionar', async (req, res) => {
         res.status(500).send("Eroare la procesarea chestionarului");
     }
 });
-app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:
-:${port}/`));
-//tema2 -eroare in caz de autetif gresita, mesaj bun venit dca e bun
-//('/autentificare'...)
-//req.cookies.username
-//   .mesajEroare
-// res.render('autentificare', {utilizator:username, mesajEroare:mesajErr})
+app.get('/deconectare', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Eroare la delogare:", err);
+        }
+        res.redirect('/'); 
+    });
+});
+app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost::${port}/`));
